78e33876ed21862fd21426d510f9e69fd775893a
Kapil Rajput <kapilraj9755@gmail.com>
2024-01-06 12:21:20 +0530

    fitst commit

---
+++ schemas.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+module.exports.productSchema = Joi.object({
+    name: Joi.string().required(),
+    img: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    desc: Joi.string().required()
+})
+
+module.exports.reviewSchema = Joi.object({
+    rating: Joi.number().min(0).max(5),
+    comment: Joi.string().required()
+})
+module.exports.CommentSchema = Joi.object({
+    comment: Joi.string().required()
+})
