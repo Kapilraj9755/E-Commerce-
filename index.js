32e353769f9f6b3a5b9772d88ab8c4c84af6edbc
Kapilraj9755 <122448699+Kapilraj9755@users.noreply.github.com>
2024-06-29 15:40:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const MongoStore = require('connect-mongo');
 const PORT = process.env.PORT || 5000;
-//const db_url = process.env.DB_URL || 'mongodb://127.0.0.1:27017/e-commerce';
+const db_url = process.env.DB_URL || 'mongodb://127.0.0.1:27017/e-commerce';
 const db_url =  'mongodb://127.0.0.1:27017/e-commerce';
 
 mongoose.connect(db_url)
