811de4ab448f3a6f803c0c2595cebf937d93a79d
Kapilraj9755 <122448699+Kapilraj9755@users.noreply.github.com>
2024-06-29 15:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const User = require('./models/user');
 const MongoStore = require('connect-mongo');
 const PORT = process.env.PORT || 5000;
 const db_url = process.env.DB_URL || 'mongodb://127.0.0.1:27017/e-commerce';
-const db_url =  'mongodb://127.0.0.1:27017/e-commerce';
+
 
 mongoose.connect(db_url)
     .then(() => { console.log('DB connected!') })
